e0c7ddaaff66ec4e4e4b2a9d4695f0fee8a4c611
_1ms <106094224+GRX005@users.noreply.github.com>
2026-05-27 14:31:51 +0200

    License headers

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
+/*
+    This file is part of the McSrvMgr project, licensed under the
+    GNU General Public License v3.0
+
+    Copyright (C) 2025-2026 _1ms (GRX005)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #![allow(non_snake_case)]
 
 mod dlMgr;
 mod usrInp;
 
 use crate::dlMgr::DlMgr;
+use console::{style, Term};
 use std::error::Error;
 use std::fs;
 use std::io::{stdin, stdout, Write};
 use std::process::{exit, Command};
-use console::{style, Term};
 use ureq::Agent;
 
 fn main() -> Result<(),Box<dyn Error>> {
